0ef594905bc0f5df57791ac427ad0454c09367f8
thomasbellina <thomasbellina98@gmail.com>
2020-04-15 18:10:48 +0200

    progettothomas

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'toDoList';
+  model ={
+    insert: ''
+  }
+  instertions=[]
+  onSubmit(insert){
+    this.instertions.push(insert);
+  }
+}
+
+
+
